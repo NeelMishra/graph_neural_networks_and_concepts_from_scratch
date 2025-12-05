9cae77acb5a28208ad7c1f00a7c245a05e380fae
NeelMishra <mishraneel99@gmail.com>
2025-12-05 09:50:37 +0530

    Added A,B,AB categories

--- Algorithms/decision_based_threshold_cascade.py
+++ Algorithms/decision_based_threshold_cascade.py
@@ -49,3 +49,71 @@ class ThresholdCascade:
                 total_nodes_changed += 1
 
         return total_nodes_changed
+    
+
+
+class ABCascadeBestResponse:
+
+    def __init__(self, G : nx.Graph, cost, reward_A, reward_B):
+        self.cost = cost
+        self.G = G
+        self.reward_A = reward_A
+        self.reward_B = reward_B
+
+        self.nodes = sorted(list(self.G.nodes()))
+        self.num_of_nodes = len(self.nodes)
+
+        self.node_to_idx = {node : idx for idx, node in enumerate(self.nodes)}
+        self.idx_to_node = {idx : node for idx, node in enumerate(self.nodes)}
+
+        self.idx_to_label = {idx : 'B' for idx in range(self.num_of_nodes)} # Initially all the nodes are have label B (non-adoption at start)
+
+    def get_neighbor_labels(self, node):
+        count_A = 0
+        count_B = 0
+        count_AB = 0
+
+        for neighbor in self.G.neighbors(node):
+            neighbor_idx = self.node_to_idx[neighbor]
+            neighbor_label = self.idx_to_label[neighbor_idx]
+
+            if neighbor_label == 'A':
+                count_A += 1
+            elif neighbor_label == 'B':
+                count_B += 1
+            else:
+                count_AB += 1
+
+        return count_A, count_B, count_AB
+    
+    def iterate(self):
+        total_nodes_changed = 0
+        for node in self.nodes:
+            node_idx = self.node_to_idx[node]
+            count_A, count_B, count_AB = self.get_neighbor_labels(node)
+            if count_A == count_B and count_A == count_AB and count_A == 0:
+                continue
+            
+            total = count_A + count_B + count_AB
+            UA = self.reward_A * (count_A + count_AB)
+            UB = self.reward_B * (count_B + count_AB)
+            UAB = self.reward_A * (count_A) + self.reward_B * (count_B) + max(self.reward_A, self.reward_B) * count_AB - self.cost 
+            
+            if UA > UB:
+
+                if UA > UAB:
+                    self.idx_to_label[node_idx] = 'A'
+                    total_nodes_changed += 1
+                else:
+                    self.idx_to_label[node_idx] = 'AB'
+                    total_nodes_changed += 1
+
+            else:
+                if UB > UAB:
+                    self.idx_to_label[node_idx] = 'B'
+                    total_nodes_changed += 1
+                else:
+                    self.idx_to_label[node_idx] = 'AB'
+                    total_nodes_changed += 1
+
+        return total_nodes_changed
